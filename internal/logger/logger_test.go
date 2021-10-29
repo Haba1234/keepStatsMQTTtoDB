c87669a2bc7289999d1ed96a1b5768676a0b3d4f
Haba1234 <b_roman@inbox.ru>
2021-10-29 19:11:03 +0300

    добавлены тесты

--- internal/logger/logger_test.go
+++ internal/logger/logger_test.go
@@ -9,16 +9,59 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestSomething(t *testing.T) {
+func TestLogger(t *testing.T) {
+	t.Parallel()
 	l, hook := test.NewNullLogger()
 	log, _ := NewLogger(config.LogConf{Level: "debug"})
+	l.Level = logrus.DebugLevel
 	log.Logger = l
 
-	log.Error("Hello error")
+	t.Run("Error", func(t *testing.T) {
+		t.Parallel()
+		log.Error("hello error")
 
-	require.Equal(t, 1, len(hook.Entries))
-	require.Equal(t, logrus.ErrorLevel, hook.LastEntry().Level)
-	require.Equal(t, "Hello error", hook.LastEntry().Message)
+		require.Equal(t, logrus.ErrorLevel, hook.LastEntry().Level)
+		require.Equal(t, "hello error", hook.LastEntry().Message)
+	})
+
+	t.Run("Errorf", func(t *testing.T) {
+		t.Parallel()
+		log.Errorf("error: %s", "hello")
+
+		require.Equal(t, logrus.ErrorLevel, hook.LastEntry().Level)
+		require.Equal(t, "error: hello", hook.LastEntry().Message)
+	})
+
+	t.Run("Info", func(t *testing.T) {
+		t.Parallel()
+		log.Info("hello info")
+
+		require.Equal(t, logrus.InfoLevel, hook.LastEntry().Level)
+		require.Equal(t, "hello info", hook.LastEntry().Message)
+	})
+
+	t.Run("Infof", func(t *testing.T) {
+		t.Parallel()
+		log.Infof("info: %s", "hello")
+
+		require.Equal(t, logrus.InfoLevel, hook.LastEntry().Level)
+		require.Equal(t, "info: hello", hook.LastEntry().Message)
+	})
+
+	t.Run("Debug", func(t *testing.T) {
+		t.Parallel()
+		log.Debug(123)
+
+		require.Equal(t, logrus.DebugLevel, hook.LastEntry().Level)
+		require.Equal(t, "123", hook.LastEntry().Message)
+	})
+
+	t.Run("Debug with parameters", func(t *testing.T) {
+		t.Parallel()
+		log.Debug("param", 123, "hello", "test")
+		require.Equal(t, logrus.DebugLevel, hook.LastEntry().Level)
+		require.Equal(t, "[hello test]", hook.LastEntry().Message)
+	})
 
 	hook.Reset()
 	require.Nil(t, hook.LastEntry())
